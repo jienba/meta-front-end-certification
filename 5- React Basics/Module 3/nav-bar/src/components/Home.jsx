f1b857b10ebc787a0bc3c38f97ae2203c091dfe3
Adama D. BA <aba@modelsis.sn>
2025-02-03 10:52:39 +0000

    Handling routing

---
+++ 5- React Basics/Module 3/nav-bar/src/components/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return (
+        <div >
+            <h1>My name is Adama Dieng BA</h1>
+            <h2>Welcome to my portfolio</h2>
+            <p>Explore and learn more about me.</p>
+        </div>
+    );
+};
+
